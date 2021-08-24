dce3d5f09d49c595492704b5d1c91f510f134b71
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-25 01:35:22 +0200

    Clase 64: Formulario

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,9 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Formulario from "./components/Formulario";
-
+import Cita from "./components/Cita";
 
 function App() {
   const[citas, setCitas] = useState([]);
+
+  // useEffect para cuando el state camboa
+  useEffect(() => {
+    console.log('listo');
+  }, [citas])
 
   // Funcion que tome las cita actuales y que agregue las nuevas
   const crearCita = cita => {
@@ -12,6 +17,18 @@ function App() {
       cita
     ])
   }
+
+  // Funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    console.log("eliminando: ", id);
+    const nuevasCitas = citas.filter(cita => cita.id != id)
+    setCitas(nuevasCitas)
+  }
+
+
+  // Mensaje condicional 
+
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas"
   
   return (
     <>
@@ -22,7 +39,14 @@ function App() {
                 <Formulario crearCita={crearCita}/>
               </div>
               <div className="one-half column">
-                2
+                <h2>{titulo}</h2>
+                {citas.map(cita => (
+                  <Cita
+                      key={cita.id} 
+                      cita={cita} 
+                      eliminarCita={eliminarCita}
+                      />
+                ))}
               </div> 
 
           </div>
