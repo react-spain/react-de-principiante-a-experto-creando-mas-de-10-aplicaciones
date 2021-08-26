874a1597bbbdcbeef6e93eee85217f9225e62bf6
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-26 16:52:28 +0200

    Clase 89

--- cotizador/src/components/Header.js
+++ cotizador/src/components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from "@emotion/styled";
+
+const ContenedorHEader = styled.header`
+    background-color: #26c6da;
+    padding: 10px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
 
 export default function Header({titulo}) {
     return (
-        <header>
-            <h1>{titulo}</h1>
-        </header>
+        <ContenedorHEader>
+            <TextoHeader>{titulo}</TextoHeader>
+        </ContenedorHEader>
     )
 }
