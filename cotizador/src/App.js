4139af5b2aeacf505d2a0b617e978c830e8c64ce
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-26 19:54:40 +0200

    93

--- cotizador/src/App.js
+++ cotizador/src/App.js
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+import Formulario from "./components/Formulario";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <>
-     <Header titulo="Cotizador de seguro"/>
-    </>
+    <Contenedor>
+      <Header titulo="Cotizador de seguro"/>
+
+      <ContenedorFormulario>
+                  <Formulario/>
+      </ContenedorFormulario>
+
+
+    </Contenedor>
+    
   );
 }
 
