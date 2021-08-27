8cc2a3c17a40fe647c3a88f47b595cb18793c049
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-28 00:04:47 +0200

    207

--- MERNTasks/cliente/src/Components/proyectos/ListadoProyectos.js
+++ MERNTasks/cliente/src/Components/proyectos/ListadoProyectos.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import Proyecto from './Proyecto'
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 export default function ListadoProyectos() {
 
-    const proyectos = [
-        {nombre: 'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Diseño de sitio Web'}
-    ]
+    // Extraer Proyectos de State Inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyectos, obtenerProyectos } = proyectosContext;
+
+    useEffect(() => {
+        obtenerProyectos();
+    }, [])
+
+    // revisar si proyecto tiene contenido
+    if(proyectos.length === 0) return null;
+
+    
 
     return (
         <ul className="listado-proyectos">
             {proyectos.map( proyecto => (
-                <Proyecto proyecto={proyecto}/>
+                <Proyecto 
+                key={ proyecto.id }
+                proyecto={proyecto}/>
             ))}
         </ul>
     )
--- MERNTasks/cliente/src/context/proyectos/proyectoState.js
+++ MERNTasks/cliente/src/context/proyectos/proyectoState.js
@@ -2,10 +2,21 @@ import React, {useReducer} from 'react';
 
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
-import {  FORMULARIO_PROYECTO  } from '../../types';
+import {  FORMULARIO_PROYECTO, OBTENER_PROYECTOS  } from '../../types';
+
+
 
 const ProyectoState = props => {
+
+    const proyectos = [
+        {id:1, nombre: 'Tienda Virtual'},
+        {id:2, nombre: 'Intranet'},
+        {id:3, nombre: 'Diseño de sitio Web'}
+    ]
+
     const initialState = {
+        proyectos : [ ],
+
         formulario:  false
     }
 
@@ -20,12 +31,22 @@ const ProyectoState = props => {
         })
     }
 
+    // Obtener los proyectos
+    const obtenerProyectos = () => {
+        dispatch({
+            type: OBTENER_PROYECTOS,
+            payload: proyectos
+        })
+    }
+
 
     return (
         <proyectoContext.Provider
             value={{
+                proyectos: state.proyectos,
                 formulario: state.formulario,
-                mostrarFormulario
+                mostrarFormulario,
+                obtenerProyectos
             }}
         >
             {props.children}
