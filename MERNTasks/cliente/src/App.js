e5f28822e932a5eb92ca3bccb65458111c861ad0
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-27 22:21:18 +0200

    203

--- MERNTasks/cliente/src/App.js
+++ MERNTasks/cliente/src/App.js
@@ -3,15 +3,19 @@ import Login from './Components/auth/Login';
 import NuevaCuenta from './Components/auth/NuevaCuenta';
 import Proyectos from './Components/proyectos/Proyectos';
 
+import ProyectoState from './context/proyectos/proyectoState';
+
 function App() {
   return (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
-            <Route exact path="/proyectos" component={Proyectos}/>
-        </Switch>
-    </Router>
+    <ProyectoState>
+      <Router>
+          <Switch>
+              <Route exact path="/" component={Login}/>
+              <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
+              <Route exact path="/proyectos" component={Proyectos}/>
+          </Switch>
+      </Router>
+    </ProyectoState>
   );
 }
 
---
+++ MERNTasks/cliente/src/context/proyectos/proyectoState.js
@@ -0,0 +1,34 @@
+import React, {useReducer} from 'react';
+
+import proyectoContext from './proyectoContext';
+import proyectoReducer from './proyectoReducer';
+
+const ProyectoState = props => {
+    const initialState = {
+        formulario:  false
+    }
+
+
+    // Dispach para ejecutar las acciones.
+    const [ state, dispatch] = useReducer(proyectoReducer, initialState)
+
+    // Serie de funciones
+
+    return (
+        <proyectoContext.Provider
+            value={{
+                formulario: state.formulario
+            }}
+        >
+            {props.children}
+        </proyectoContext.Provider>
+    )
+
+}
+
+export default ProyectoState;
+
+
+
+
+
