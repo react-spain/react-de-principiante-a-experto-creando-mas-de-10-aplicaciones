f396c7460b51eb7a82edcbd8a6708554af81a64d
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-27 16:50:16 +0200

    195

--- MERNTasks/cliente/src/Components/proyectos/Proyectos.js
+++ MERNTasks/cliente/src/Components/proyectos/Proyectos.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Sidebar from '../loyout/Sidebar'
 
 export default function Proyectos() {
     return (
-        <h1>Desde Proyectos</h1>
+        <div className="contenedor-app">
+            <Sidebar />
+
+            <div className="seccion-principal"> 
+                <main>
+                        <div className="contenedor-tareas">
+
+                        </div>
+                </main>
+            </div>
+        </div>
     )
 }
