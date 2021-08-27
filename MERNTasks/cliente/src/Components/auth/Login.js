b69a30cb0de6eba81eacc2167f8137ad9da4947e
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-27 15:53:04 +0200

    192

--- MERNTasks/cliente/src/Components/auth/Login.js
+++ MERNTasks/cliente/src/Components/auth/Login.js
@@ -1,7 +1,48 @@
 import React from 'react'
 
 export default function Login() {
+    const onChange = () => {
+
+    }
+
     return (
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sessión</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu email"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesión"
+                        />
+                    </div>
+
+                </form>
+            </div>
+        </div>
     )
 }
