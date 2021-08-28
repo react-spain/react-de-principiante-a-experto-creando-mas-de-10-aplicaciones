9cf116944ac2c89c786285309a3a3867117ec47a
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-29 01:31:58 +0200

    Clase 233 MONGODB

--- MERNTasks/servidor/index.js
+++ MERNTasks/servidor/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // crear el servidor
 const app = express();
+
+
+// Conectar a la BD
+conectarDB();
+
+// Connect to mongodb
+// const URI = process.env.DB_MONGO
+// mongoose.connect("mongodb+srv://admin:x1234567890@cluster0.9pk6w.mongodb.net/merntasks", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, err => {
+//     if(err) throw err;
+//     console.log("Connected to mongodb")
+// })
 
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
