d8ba77ffba9a37d085f43f53262d1186ecfa6d69
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-28 11:15:13 +0200

    Clase 212

--- MERNTasks/cliente/src/Components/tareas/FormTarea.js
+++ MERNTasks/cliente/src/Components/tareas/FormTarea.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 export default function FormTarea() {
+
+    // Extraer si un proyecto esta activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyectos seleccionado
+    if(!proyecto) return null
+
+    // Array destructuring
+    const [proyectoActual] = proyecto
+
     return (
         <div className="formulario">
             <form>
