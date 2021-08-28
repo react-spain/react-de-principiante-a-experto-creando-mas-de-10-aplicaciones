58fa1e8527bc68fa4e494bb3a2a805c2c5f05055
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-28 11:03:01 +0200

    Clase 210

--- MERNTasks/cliente/src/context/proyectos/proyectoState.js
+++ MERNTasks/cliente/src/context/proyectos/proyectoState.js
@@ -7,7 +7,8 @@ import {
             FORMULARIO_PROYECTO, 
             OBTENER_PROYECTOS, 
             AGREGAR_PROYECTO,
-            VALIDAR_FORMULARIO
+            VALIDAR_FORMULARIO,
+            PROYECTO_ACTUAL
         } from '../../types';
 
 
@@ -23,7 +24,8 @@ const ProyectoState = props => {
     const initialState = {
         proyectos : [ ],
         formulario:  false,
-        errorformulario: false
+        errorformulario: false,
+        proyecto: null
     }
 
 
@@ -66,17 +68,29 @@ const ProyectoState = props => {
     }
 
 
+    // Selecciona el proyecto que se dio click
+    const proyectoActual = proyectoId => {
+        dispatch({
+            type: PROYECTO_ACTUAL,
+            payload: proyectoId
+        })
+
+    }
+
+
     return (
         <proyectoContext.Provider
             value={{
                 proyectos: state.proyectos,
                 formulario: state.formulario,
                 errorformulario: state.errorformulario,
+                proyecto: state.proyecto,
 
                 mostrarFormulario,
                 obtenerProyectos,
                 agregarProyecto,
-                mostrarError
+                mostrarError,
+                proyectoActual
                 
             }}
         >
