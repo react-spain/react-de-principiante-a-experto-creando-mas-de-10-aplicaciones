2c83749e58b726dc23e7d95327262d958f65898c
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-29 02:31:46 +0200

    Clase 241

--- MERNTasks/servidor/controllers/usuarioController.js
+++ MERNTasks/servidor/controllers/usuarioController.js
@@ -1,3 +1,39 @@
-exports.crearUsuario = (req, res) => {
-    console.log(req.body)
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
+exports.crearUsuario = async (req, res) => {
+
+    // revisar si hay errores
+    const errores = validationResult(req)
+    if(!errores.isEmpty()) {
+        return res.status(400).json({errores: errores.array()})
+    }
+
+    const {email, password} = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({email});
+        if(usuario){
+            res.status(400).json({
+                msg: 'El usuario ya existe'
+            });
+        }
+
+        // crea el usuario
+        usuario = new Usuario(req.body);
+
+        // hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password =  await bcryptjs.hash(password, salt)
+
+        // guarda
+        await usuario.save()
+        res.status(200).json({
+            msg: 'Usuario creado correctamente'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
--- MERNTasks/servidor/routes/usuarios.js
+++ MERNTasks/servidor/routes/usuarios.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const { check } = require('express-validator');
 const usuarioController = require('../controllers/usuarioController')
 
 // Crea un usuario
 // api/usuarios
 router.post('/', 
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email valido').isEmail(),
+        check('password', 'El password debe ser al menos 6 caracteres').isLength({min:6})
+    ],
     usuarioController.crearUsuario
 );
 
