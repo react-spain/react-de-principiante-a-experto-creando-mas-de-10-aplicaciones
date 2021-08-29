3bd0eaf679c3169f08866813778496a9c68c92b0
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-29 12:12:24 +0200

    Clase 253

--- MERNTasks/servidor/controllers/proyectoController.js
+++ MERNTasks/servidor/controllers/proyectoController.js
@@ -1,6 +1,13 @@
 const Proyecto = require('../models/Proyecto');
+const { validationResult  } = require('express-validator');
 
 exports.crearProyecto =  async ( req, res ) => {
+    // revisar si hay errores
+    const errores = validationResult(req)
+    if(!errores.isEmpty()) {
+        return res.status(400).json({errores: errores.array()})
+    }
+
     try {
 
         const proyecto = new Proyecto(req.body);
@@ -11,6 +18,64 @@ exports.crearProyecto =  async ( req, res ) => {
         proyecto.save();
         res.json(proyecto);
         
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+// Obteniendo todos los proyectos del usuario actual
+exports.obtenerProyectos = async (req, res) => {
+    try {
+        const proyectos = await Proyecto.find({ creador : req.usuario.id }).sort({ creado: -1 });
+        res.json(proyectos);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+// Actualiza un proyecto
+exports.actualizarProyecto = async  (req, res) => {
+    // revisar si hay errores
+    const errores = validationResult(req)
+    if(!errores.isEmpty()) {
+        return res.status(400).json({errores: errores.array()})
+    }
+
+    // extraer la info
+
+    try {
+        const {nombre} = req.body
+        const nuevoProyecto = {};
+        if(nombre) {
+            nuevoProyecto.nombre =  nombre
+        }
+
+        // revisar el ID
+        idProyecto = req.params.id
+        let proyecto = await Proyecto.findById(idProyecto);
+
+        // Si existe
+        if(!proyecto) {
+            res.status(404).send('Proyecto no Encontrado');
+        }
+
+        // verificar el creador
+        if(proyecto.creador.toString() != req.usuario.id) {
+            res.status(401).send('No autorizado');
+        }
+
+        proyecto = await Proyecto.findOneAndUpdate(
+                    {_id: idProyecto}, 
+                    { $set: nuevoProyecto },
+                    {new: true})
+
+        res.json({proyecto});
+
+
+        // actualizar
+        res.json(proyecto);
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
--- MERNTasks/servidor/routes/proyectos.js
+++ MERNTasks/servidor/routes/proyectos.js
@@ -2,18 +2,30 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 
 // Crear Proyectos
 // api/proyectos
 router.post('/',
     auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty(),
+    ],
     proyectoController.crearProyecto
 )
 
 router.get('/',
     auth,
-    proyectoController.crearProyecto
+    proyectoController.obtenerProyectos
+)
+
+router.put('/:id',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty(),
+    ],
+    proyectoController.actualizarProyecto
 )
 
 module.exports = router;
