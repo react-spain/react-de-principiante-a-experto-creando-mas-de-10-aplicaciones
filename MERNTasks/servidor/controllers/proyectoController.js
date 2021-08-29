0e3c95181bb5858d15284e0b932b565d19d0cce6
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-08-29 03:27:24 +0200

    Clase 248 - Api proyecto

--- MERNTasks/servidor/controllers/proyectoController.js
+++ MERNTasks/servidor/controllers/proyectoController.js
@@ -1,3 +1,14 @@
+const Proyecto = require('../models/Proyecto');
+
 exports.crearProyecto =  async ( req, res ) => {
-    
+    try {
+
+        const proyecto = new Proyecto(req.body);
+        proyecto.save();
+        res.json(proyecto);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
